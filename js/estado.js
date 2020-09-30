a02d4fa40f9ff924a78296fc88c8d01d36a00d00
Bianca-lemesp <bianca.lemesp@hotmail.com>
2020-09-30 15:13:02 -0300

    Primeiro commit

---
+++ js/estado.js
@@ -0,0 +1,159 @@
+let iframe = document.querySelector('iframe');
+
+function ac() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src', 'estados/ac.html');
+}
+
+function al() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src', 'estados/al.html');
+}
+
+function am() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src', 'estados/am.html');
+}
+
+function ap() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src', 'estados/ap.html');
+}
+
+function ba() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/ba.html'); 
+    
+}
+
+function ce() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/ce.html'); 
+    
+}
+
+function df() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/df.html'); 
+    
+}
+
+function es() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/es.html'); 
+    
+}
+
+function go() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/go.html'); 
+    
+}
+
+function ma() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/ma.html'); 
+    
+}
+
+function mt() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/mt.html'); 
+    
+}
+
+function ms() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/ms.html'); 
+    
+}
+
+function mg() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/mg.html'); 
+    
+}
+
+function pa() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/pa.html'); 
+    
+}
+
+function pb() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/pb.html'); 
+    
+}
+
+function pr() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/pr.html'); 
+    
+}
+
+function pe() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/pe.html'); 
+    
+}
+
+function pi() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/pi.html'); 
+    
+}
+
+function rj() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/rj.html'); 
+    
+}
+
+function rn() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/rn.html'); 
+    
+}
+
+function rs() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/rs.html'); 
+    
+}
+
+function ro() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/ro.html'); 
+    
+}
+
+function rr() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/rr.html'); 
+    
+}
+
+function sc() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/sc.html'); 
+    
+}
+
+function sp() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/sp.html'); 
+    
+}
+
+function se() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/se.html'); 
+    
+}
+
+function to() {
+    iframe.removeAttribute('src');
+    iframe.setAttribute('src','estados/to.html'); 
+    
+}
